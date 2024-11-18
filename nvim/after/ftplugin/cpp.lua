6180050f3f6aaf35e291a780750107fda43ed946
Yash Dudam <yashasvi.dudam@gmail.com>
2024-11-18 11:11:13 +1100

    initial flawed config

---
+++ nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,10 @@
+local opt_local = vim.opt_local
+
+opt_local.tabstop = 2 -- 2 spaces for tabs
+opt_local.shiftwidth = 2 -- 2 spaces for indent width
+
+-- opt_local.tabstop = 4 -- 4 spaces for tabs
+-- opt_local.shiftwidth = 4 -- 4 spaces for indent width
+
+opt_local.expandtab = true -- expand tab to spaces
+opt_local.autoindent = true -- copy indent from current line when starting new one
---
+++ nvim/lua/yash/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+return {
+  -- "navarasu/onedark.nvim",
+  -- priority = 1000, -- make sure to load this before all the other start plugins
+  -- config = function()
+  --   require("onedark").setup({
+  --     style = "deep",
+  --   })
+  --   vim.cmd([[colorscheme onedark]])
+  -- end,
+
+  -- "olimorris/onedarkpro.nvim",
+  -- priority = 1000,
+  -- config = function()
+  --   vim.cmd([[colorscheme onedark]])
+  -- end,
+
+  "luisiacc/gruvbox-baby",
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme gruvbox-baby]])
+  end,
+
+  -- "rebelot/kanagawa.nvim",
+  -- priority = 1000,
+  -- config = function()
+  --   vim.cmd([[colorscheme kanagawa-wave]])
+  -- end,
+
+  --
+}
